eee0e1dfc512a6411cffcfc4114aaeeff8cc9292
Eihapkaram <eihapkaram9090@gmail.com>
2026-01-08 06:14:48 -0800

    ver6

--- src/components/BannerCom.jsx
+++ src/components/BannerCom.jsx
@@ -15,28 +15,37 @@ function banner() {
         interval={3000}
       >
         <Carousel.Item>
-          <h3 className="title">First slide label</h3>
           <Carousel.Caption className="text1">
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            <Btn text="عرض التفاصيل" backcolor="white" color="black" />
+            <h4 className="title1">مشروع</h4>
+            <h3 className="title2">K-110</h3>
+            <p className="title3">الرياض - الزهراء</p>
+            <div className="btndiv">
+              <Btn text="عرض التفاصيل" backcolor="white" color="black" />
+            </div>
           </Carousel.Caption>
           <ExampleCarouselImage text="First slide" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <h3 className="title">First slide label</h3>
           <Carousel.Caption className="text1">
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            <Btn text="عرض التفاصيل" backcolor="white" color="black" />
+            <h4 className="title1">مشروع</h4>
+            <h3 className="title2">K-110</h3>
+            <p className="title3">الرياض - الزهراء</p>
+            <div className="btndiv">
+              <Btn text="عرض التفاصيل" backcolor="white" color="black" />
+            </div>
           </Carousel.Caption>
           <ExampleCarouselImage2 text="Second slide" />
         </Carousel.Item>
 
         <Carousel.Item>
-          <h3 className="title">First slide label</h3>
           <Carousel.Caption className="text1">
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            <Btn text="عرض التفاصيل" backcolor="white" color="black" />
+            <h4 className="title1">مشروع</h4>
+            <h3 className="title2">K-110</h3>
+            <p className="title3">الرياض - الزهراء</p>
+            <div className="btndiv">
+              <Btn text="عرض التفاصيل" backcolor="white" color="black" />
+            </div>
           </Carousel.Caption>
           <ExampleCarouselImage3 text="Third slide" />
         </Carousel.Item>
